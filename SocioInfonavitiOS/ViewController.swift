f3f5cad70a82171266731167f81ac015e3b29dff
pepeMalpica <pepecode6@gmail.com>
2020-09-02 12:57:26 -0500

    Initial Commit

---
+++ SocioInfonavitiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocioInfonavitiOS
+//
+//  Created by pepe on 02/09/20.
+//  Copyright © 2020 pepeMalpica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
